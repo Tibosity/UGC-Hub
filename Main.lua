fb7ac901988b404ea5b43b2e15d4c4f5fc0aad7e
Tibosity <89561185+Tibosity@users.noreply.github.com>
2023-09-10 15:22:06 -0400

    Update Main.lua

---
+++ Main.lua
@@ -0,0 +1,99 @@
+if game.GameId == 12888650105 then -- Anime Battlegrounds Y
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Anime%20Battlegrounds%20Y.lua",true))()
+end
+
+if game.GameId == 13370783664 then -- Anime Energy Clash Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Anime%20Energy%20Clash%20Simulator.lua",true))()
+end
+
+if game.GameId == 13264238461 then -- ANIME HATCHERS
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Anime%20Hatchers.lua",true))()
+end
+
+if game.GameId == 14508327597 then -- Cart Ride
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Cart%20Ride.lua",true))()
+end
+
+if game.GameId == 14047294951 then -- Chest Hero Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Chest%20Hero%20Simulator.lua",true))()
+end
+
+if game.GameId == 12979480582 then -- Don't Make The Rock Angry
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Don’t%20Make%20The%20Rock%20Angry.lua",true))()
+end
+
+if game.GameId == 13978342818 then -- ESCAPE GORDAN RAMSAY
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/ESCAPE%20GORDAN%20RAMSAY!.lua",true))()
+end
+
+if game.GameId == 14527687035 then -- Every Second Get +1 Speed
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Every%20Second%20Get%20%2B1%20Speed.lua",true))()
+end
+
+if game.GameId == 11063612131 then -- Every Second You Get +1 Jump Power
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Every%20Second%20You%20Get%20%2B1%20Jump%20Power.lua",true))()
+end
+
+if game.GameId == 13973120326 then -- Grimace Race
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Grimace%20Race.lua",true))()
+end
+
+if game.GameId == 14656843302 then -- Math Block Race
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Math%20Block%20Race.lua",true))()
+end
+
+if game.GameId == 13959063215 then -- Mine Block Tycoon
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Mine%20Block%20Tycoon.lua",true))()
+end
+
+if game.GameId == 13778544303 then -- Motorcycle Race
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Motorcycle%20Race.lua",true))()
+end
+
+if game.GameId == 9802068147 then -- Museum Tycoon
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Museum%20Tycoon.lua",true))()
+end
+
+if game.GameId == 14518789199 then -- Obby But You're on a Pogo Stick
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Obby%20But%20You're%20on%20a%20Pogo%20Stick.lua",true))()
+end
+
+if game.GameId == 14034480512 then -- Obby But You're on a Jetpack
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Obby%20But%20You’re%20on%20a%20Jetpack.lua",true))()
+end
+
+if game.GameId == 14350188056 then --Pet Race
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Pet%20Race.lua",true))()
+end
+
+if game.GameId == 13683398813 then -- Save A Friend
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Save%20A%20Friend.lua",true))()
+end
+
+if game.GameId == 8908228901 then -- Sharkbite 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/SharkBite%202.lua",true))()
+end
+
+if game.GameId == 13748085494 then -- Shoot a Friend Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Shoot%20a%20Friend%20Simulator.lua",true))()
+end
+
+if game.GameId == 9049840490 then -- Sonic Speed Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Sonic%20Speed%20Simulator.lua",true))()
+end
+
+if game.GameId == 13274636440 then -- Supermarket Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Supermarket%20Simulator.lua",true))()
+end
+
+if game.GameId == 12641272458 then -- Teamwork Puzzle 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Teamwork%20Puzzles%202.lua",true))()
+end
+
+if game.GameId == 9376792433 then -- Ultimate 17+ Obby
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Ultimate%2017%2B%20Obby.lua",true))()
+end
+
+if game.GameId == 13780662126 then -- Yeet a Plane Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tibosity/UGC-Hub/main/Games/Yeet%20a%20Plane%20Simulator.lua",true))()
+end
